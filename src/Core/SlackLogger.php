ec1f0a1f797fcf8e59e47478c7ec4f0f38d74b08
Gentrit Abazi <35135482+gentritabazi01@users.noreply.github.com>
2020-08-14 11:18:48 +0200

    Update SlackLogger.php

--- src/Core/SlackLogger.php
+++ src/Core/SlackLogger.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace one2tek\larapi\Core;
+namespace one2tek\larapiComponents\Core;
 
 use Monolog\Logger;
 use Monolog\Handler\SlackWebhookHandler;
@@ -16,16 +16,26 @@ class SlackLogger
      */
     public function __invoke($logger)
     {
-        foreach($logger->getHandlers() as $handler) {
-            if($handler instanceof SlackWebhookHandler) {
-                $handler->pushProcessor(function($record) {
+        foreach ($logger->getHandlers() as $handler) {
+            if ($handler instanceof SlackWebhookHandler) {
+                $handler->pushProcessor(function ($record) {
                     $record['extra']['App Env'] = config('app.env');
                     $record['extra']['Date & Time'] = date('Y-m-d H:i:s');
-                    if(Auth::check()) {
+                    if (Auth::check()) {
                         $record['extra']['User Email'] = Auth::user()->email;
                     }
                     if (request()->getPathInfo()) {
                         $record['extra']['Request URL'] = asset(request()->getPathInfo());
+                    }
+                    $record['extra']['Method'] = request()->getMethod();
+                    if (strlen(json_encode(request()->all()))) {
+                        $record['extra']['Parameters'] = json_encode(request()->all());
+                    }
+                    if (isset($_SERVER['HTTP_ORIGIN'])) {
+                        $record['extra']['Origin'] = $_SERVER['HTTP_ORIGIN'];
+                    }
+                    if (isset($_SERVER['HTTP_REFERER'])) {
+                        $record['extra']['Referer'] = $_SERVER['HTTP_REFERER'];
                     }
 
                     return $record;
