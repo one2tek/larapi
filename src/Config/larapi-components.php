a550f3a9bf79c0e959fc6a9f1ea42619d149fc95
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-19 23:11:17 +0100

    Removed RouteServiceProvider!

--- src/Config/larapi-components.php
+++ src/Config/larapi-components.php
@@ -1,27 +1,5 @@
 <?php
 
 return [
-    'namespaces' => [
-        'Api' => base_path() . DIRECTORY_SEPARATOR . 'api',
-        'Infrastructure' => base_path() . DIRECTORY_SEPARATOR . 'infrastructure'
-    ],
-
-    'protection_middleware' => [
-        'auth:api'
-    ],
-
-    'extra_routes' => [
-        'routes_v1' => [
-            'middleware' => [],
-            'namespace' => 'Controllers\V1',
-            'prefix' => 'v1'
-        ],
-        'routes_v2' => [
-            'middleware' => [],
-            'namespace' => 'Controllers\V2',
-            'prefix' => 'v2'
-        ]
-    ],
-
     'slack_formatter' => '\Infrastructure\Formatters\SlackFormatter'
 ];
