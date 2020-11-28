1cc18989e2a78dfb41e06bf57367621bb0ee688c
Gentrit Abazi <gentritabazi01@gmail.com>
2020-11-29 00:49:17 +0100

    Added ApiException with formatters.

--- src/Config/larapi.php
+++ src/Config/larapi.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    //
+    'exceptions_formatters' => [
+        Throwable::class => one2tek\larapi\ExceptionFormatter::class,
+        SymfonyException\UnprocessableEntityHttpException::class => one2tek\larapi\UnprocessableEntityHttpExceptionFormatter::class
+    ]
 ];
